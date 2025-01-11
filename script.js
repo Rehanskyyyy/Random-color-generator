9f5e43eadf5709f32da41e3ed5b2fc0d50734fbd
Rehanskyyyy <moh.rehan5292@gmail.com>
2025-01-11 23:38:09 +0530

    Do some modification in Website

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ function getRandomColor() {
     return `rgb(${val1}, ${val2}, ${val3})`
 }
 
+function addPressEffect() {
+    button.style.transform = "scale(0.95)";
+    button.style.transition = "transform 0.1s ease";
+}
+
+function removePressEffect() {
+    button.style.transform = "scale(1)";
+}
+
+// Handle desktop and touchscreen events
+button.addEventListener("mousedown", addPressEffect);
+button.addEventListener("mouseup", removePressEffect);
+button.addEventListener("mouseout", removePressEffect); // Reset if mouse leaves the button
+
+button.addEventListener("touchstart", addPressEffect);
+button.addEventListener("touchend", removePressEffect);
+
 button.addEventListener("click", () => {
     document.querySelector(".box").innerHTML = "Click again and again to keep changing colors"
     document.body.style.backgroundColor = getRandomColor()
